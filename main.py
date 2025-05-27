fd78d0b3a2427e9051bd4d80997d7221afec78d7
Leonardo Stall <leonardo.stall.01tec@gmail.com>
2025-05-27 00:00:16 -0300

    Melhoria - exportar PDF
    Adição da possibilidade de exportar PDF

--- main.py
+++ main.py
@@ -892,7 +892,8 @@ async def minhas_reservas(request: Request):
                 s.Imagem AS imagem,
                 ts.Tipo AS tipo_sala,
                 s.Capacidade AS capacidade_sala,     
-                s.Descricao AS descricao_sala
+                s.Descricao AS descricao_sala,
+                s.Valor_Hora AS valor_hora
             FROM locacao_loca r
             JOIN salas s ON r.fk_salas_ID_Sala = s.ID_Sala
             JOIN tipo_sala ts ON s.fk_tipo_sala_ID_Tipo_Sala = ts.ID_Tipo_Sala
